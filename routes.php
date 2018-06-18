960744068f13b04e71a38cab950b1460612ece93
Bulle <Bulle>
2018-06-18 06:29:00 +0200

    Ebook 2018
    Work for exam

---
+++ routes.php
@@ -0,0 +1,25 @@
+<?php 
+
+$routes = [
+	
+	"show_books",
+
+	"index_books",
+
+	"show_authors",
+
+	"index_authors",
+
+	"show_editors",
+
+	"index_editors",
+
+	"show_types",
+
+	"index_types",
+
+	"index_login",
+
+	"index_addBook"
+
+];
